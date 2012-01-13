d2d03c825401fbbfbffe4beb89853f27f82f07aa
myeisha <myeisha@quasiparticle.net>
2012-01-13 10:29:29 +0100

    generate deps with cpp, not cxx

--- functions.mk
+++ functions.mk
@@ -1,6 +1,6 @@
 # usage: $(call make-depend,source-file,object-file,depend-file)
 define make-depend
-	@$(CXX) -MM -MP -MT $2 $(CFLAGS) $1 | sed -e 's@^\(.*\)\.o:@\1.d \1.o:@' > $3
+	@$(CPP) -MM -MP -MT $2 $(CFLAGS) $1 | sed -e 's@^\(.*\)\.o:@\1.d \1.o:@' > $3
 endef
 
 define dirname
